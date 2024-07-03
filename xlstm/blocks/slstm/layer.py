cecbd50eb8406d98a139afc5d69e03732cf59964
Maximilian Beck <maximilian.beck@nx-ai.com>
2024-07-03 13:51:53 +0200

    fix slstm return_last_state.

--- xlstm/blocks/slstm/layer.py
+++ xlstm/blocks/slstm/layer.py
@@ -132,11 +132,11 @@ class sLSTMLayer(nn.Module):
 
         if self.config.conv1d_kernel_size > 0:
             if return_last_state:
-                x_conv = self.conv1d(x, conv_state, return_last_state=return_last_state)
-            else:
                 x_conv, conv_state = self.conv1d(
                     x, conv_state, return_last_state=return_last_state
                 )
+            else:
+                x_conv = self.conv1d(x, conv_state, return_last_state=return_last_state)
             x_conv = self.conv_act_fn(x_conv)
         else:
             x_conv = x
